a7e580a06d98686f90f6a73abdf71c7493d67be6
hajerKhazri <khazri.hajer@esprit.tn>
2024-11-07 11:16:53 -0800

    first commit

---
+++ src/Entities/Aquatic.java
@@ -0,0 +1,53 @@
+package Entities;
+
+public class Aquatic {
+    public abstract non-sealed class Aquatic extends Animal {
+
+        protected String habitat;
+
+        public Aquatic() {
+        }
+
+        public Aquatic(String family, String name, int age, boolean isMammal, String habitat) {
+            super(family, name, age, isMammal);
+            this.habitat = habitat;
+        }
+
+
+        public abstract void swim();
+
+        @Override
+        public String toString() {
+            return super.toString() + ", habitat:" + habitat;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) return true;
+            if (obj == null) return false;
+            if (obj instanceof Aquatic aquatic) {
+                return aquatic.habitat.equals(habitat) && aquatic.getName().equals(super.getName()) && aquatic.getAge() == super.getAge();
+            }
+            return false;
+        }
+    }
+}
+@Override
+public void eatMeat(Food food) {
+    if (food == Food.MEAT) {
+        System.out.println("L'animal aquatique mange de la viande.");
+    } else {
+        System.out.println("L'animal aquatique ne mange pas cet aliment.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Aquatic aquaticAnimal;
+        aquaticAnimal.eatMeat(Food.MEAT);
+        aquaticAnimal.eatMeat(Food.PLANT);
+        aquaticAnimal.eatMeat(Food.BOTH);
+    }
+}
